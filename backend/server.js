11004594e2e2205cb17be95c929f43f5e6a835c2
kubraacelik <kkbra.celik92@gmail.com>
2024-10-16 13:05:29 +0300

    added userRouter

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,24 @@
-import express from "express"
-import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
 
 //! .env dosyasını projenin içinde kullanır
 dotenv.config();
 
-// app config 
+// app config
 //! app: Express uygulamasını başlatan değişken
 //! port: Sunucunun hangi port üzerinde çalışacağını belirler
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
 
 // middleware
 //! sunucuya gelen JSON formatındaki istek verilerini otomatik olarak ayrıştırır
 //! CORS izinlerini sağlar
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // db connection
 //! veritabanına bağlandı
@@ -25,16 +26,20 @@ connectDB();
 
 // api endpoints
 //! foodRouter'ı /api/food yoluna yönlendiren bir middleware ekler
-app.use("/api/food",foodRouter)
+app.use("/api/food", foodRouter);
+
 //! resim uploads/sample.jpg olarak kaydedilmişse tarayıcıda images/sample.jpg adresinden erişilebilir
-app.use("/images",express.static("uploads"))
+app.use("/images", express.static("uploads"));
+
+//! userRouter'ı /api/user yoluna yönlendiren bir middleware ekler
+app.use("/api/user", userRouter);
 
 //! "http://localhost:4000/" adresine gelen isteklerde "API Working" yanıtı döner
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
 
 //! Sunucunun belirtilen port üzerinde çalışmasını sağlar
-app.listen(port,()=>{
-    console.log(`Server started on http://localhost:${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
